c9034c80506e56b7373f7f6375faeadaf5ea79b7
adgx <antonino.d.gregorio@gmail.com>
2025-12-14 11:35:19 +0100

    fixed fullscreen

--- src/managers/windowManager.cpp
+++ src/managers/windowManager.cpp
@@ -56,29 +56,18 @@ namespace SpaceEngine
     {
         SPACE_ENGINE_TRACE("App - set up GLFW");
         glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         window = glfwCreateWindow(width, height, "Spaceship", NULL, NULL);
-        
-        //setup window
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        glfwSetWindowSizeLimits(window, SPACE_ENGINE_MIN_RES_W, SPACE_ENGINE_MIN_RES_H, GLFW_DONT_CARE, GLFW_DONT_CARE);
-        glfwSetWindowAspectRatio(window, 16, 9);
-        glfwSetWindowMaximizeCallback(window, window_maximize_callback);
-
-        //saving the position where is created
-        glfwGetWindowPos(window, &xpos, &ypos);
-
         if(window == nullptr)
         {
             SPACE_ENGINE_ERROR("Failed to create GLFW window");
             return false;
         }
-
         glfwMakeContextCurrent(window);
-
+        
         int version = gladLoadGL(glfwGetProcAddress);
 
         if(version == 0)
@@ -86,12 +75,22 @@ namespace SpaceEngine
             SPACE_ENGINE_ERROR("Failed to initilize OpenGL Context");
             return false;
         }
+        
+        //setup window
+        glfwSetWindowSizeLimits(window, SPACE_ENGINE_MIN_RES_W, SPACE_ENGINE_MIN_RES_H, GLFW_DONT_CARE, GLFW_DONT_CARE);
+        glfwSetWindowAspectRatio(window, 16, 9);
+        //callbacks
+        glfwSetWindowMaximizeCallback(window, window_maximize_callback);
+        glfwSetFramebufferSizeCallback(window, window_resize_callback);
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        //saving the position where is created
+        glfwGetWindowPos(window, &xpos, &ypos);
+        
+        glViewport(0, 0, w, h); 
         //set the OpenGL depth buffer
         glEnable(GL_DEPTH_TEST);
         //set the OpenGL framebuffer
-        //glViewport(0, 0, SPACE_ENGINE_MIN_RES_W, SPACE_ENGINE_MIN_RES_H); 
-        //callbacks
-        glfwSetFramebufferSizeCallback(window, window_resize_callback);
 
         SPACE_ENGINE_INFO("WindowManager - GLFW setup done");
         return true;
@@ -137,13 +136,11 @@ namespace SpaceEngine
 
     void window_resize_callback(GLFWwindow* window, int width, int height)
     {
-        SPACE_ENGINE_DEBUG("Resize frame buffer w{} h{}", width, height);
-        if((width > 1900 && width < 1940) && (height>980 && height<1040))
+        if(!WindowManager::fullScreenState)
         {
-            width = 1920;
-            height = 1080;
+            SPACE_ENGINE_DEBUG("Resize frame buffer w{} h{}", width, height);
+            glViewport(0, 0, width, height);
         }
-        glViewport(0, 0, width, height);
     }
 
     //callbacks
@@ -151,13 +148,13 @@ namespace SpaceEngine
     {
         if (maximized)
         {
-            // The window was maximized
-            WindowManager::fullScreenState = true;
-            WindowManager::monitor = get_window_monitor(window);
-            const GLFWvidmode* mode = glfwGetVideoMode(WindowManager::monitor);
-            
-            glfwSetWindowMonitor(window, WindowManager::monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
-            //glViewport(0, 0, SPACE_ENGINE_MAX_RES_W, SPACE_ENGINE_MAX_RES_H);
+            //glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
+                // The window was maximized
+                WindowManager::fullScreenState = true;
+                WindowManager::monitor = get_window_monitor(window);
+                const GLFWvidmode* mode = glfwGetVideoMode(WindowManager::monitor);
+                glfwSetWindowMonitor(window, WindowManager::monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+                glViewport(0, 0, mode->width, mode->height);
         }
 
     }
